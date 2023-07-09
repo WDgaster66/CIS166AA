ff773d0891dd59ed0f6e5744ad55303966233578
WDgaster66 <50276790+WDgaster66@users.noreply.github.com>
2023-07-09 10:58:44 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-main
+function playGame() {
+	var randomNumber = Math.floor(Math.random() * 100) + 1; // get a random number between 1 and 100
+	var min = 50; // set the minimum value
+	var max = 75; // set the maximum value
+	if (randomNumber > min && randomNumber < max) { // check if the random number is within the range
+		result.textContent = "Your Number: " + randomNumber + ", You Won!"; // display a winning message
+	} else {
+		result.textContent = "Your Number: " + randomNumber + ", Keep Trying!"; // display a losing message
+	}
+}
