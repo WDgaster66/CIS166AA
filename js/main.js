94ddc84a864e1375bfe6be9603196cb952d3db07
WDgaster66 <50276790+WDgaster66@users.noreply.github.com>
2023-07-08 23:54:30 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-main
+$(document).ready(function() {
+    $("#slideshow > div:gt(0)").hide(); // hide all divs except the first one
+    setInterval(function() {
+        $("#slideshow > div:first") // get the first div
+            .fadeOut(1000) // fade out
+            .next() // get the next div
+            .fadeIn(1000) // fade it in
+            .end() // go back to the first div
+            .appendTo("#slideshow"); // move it to the end of the slideshow
+    }, 3000); // repeat every 3 seconds
+});
